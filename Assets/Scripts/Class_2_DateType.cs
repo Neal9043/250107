5b49cee0bb245a02c0e71af7f5223f8f258e057c
Neal9043 <vm644u04@gmail.com>
2025-01-08 20:52:44 +0800

    課程_2_變數類型

---
+++ Assets/Scripts/Class_2_DateType.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.Contracts;
+using UnityEngine;
+
+// 三條斜線是摘要 XML語言 用來做簡短說明
+/// <summary>
+/// 認識 C# 資料類型
+/// </summary>
+public class Class_2_DateType : MonoBehaviour
+{
+    //變數語法 :
+    //修飾詞 資料類型 變數名稱 指定 預設值;
+    //整數 : int 儲存沒有小數點的數值
+    //浮點 : float 儲存有小數點的數值
+    //字串 : string 儲存文字 必須使用雙引號
+    //布林值  : bool 只有 true false 兩個詞
+    public float moveSpeed = 3.5f; //浮點必須添加 f 後綴
+    public float turnSpeed = 30.7F; //大小F都可
+    public string character = "提摩";
+    public bool isDead = false; //是否死亡;否
+    public bool gameOver = true; //是否結束;是
+    public int counnt = 7;
+
+}
--- Assets/Scripts/class1Variable.cs
+++ Assets/Scripts/class1Variable.cs
@@ -8,8 +8,9 @@ using Unity.VisualScripting;
 using UnityEngine;
 
 //修飾詞 (類別名稱、類別關鍵字
+// 腳本後添加MonoBehaviour 可讓腳本掛在unity
+public class class1Variables : MonoBehaviour
 
-public class class1Variable
 {
     //程式內的括號都是成對出現 () [] {} '' ""
     // class {} 此類別的程式內容
@@ -20,9 +21,14 @@ public class class1Variable
 
     //變數語法:
     //變數資料類型 變數名稱;
+    //變數資料類型 變數名稱 指定 預設值 ; - 可省略
     //資料類型:告訴記憶體可放那些資料,例如整數int
     //變數名稱;習慣用小寫命名,規則與腳本名稱相同
-
+    //修飾詞 : 公開:允許其他類別讀取pubilc
+    //私人 : private 禁止其他人讀取
     //int coin; 定義金幣變數
+
+    public int coin = 500;
+    private int level = 3;
 }
 
